f0bfd09dadc9aea0f933728038ce1c3a01097c57
xopyip <xopyip@gmail.com>
2022-05-22 21:38:28 +0200

    add context menu to PrintCell

--- src/main/java/pl/baluch/stickerprinter/data/PrintCell.java
+++ src/main/java/pl/baluch/stickerprinter/data/PrintCell.java
@@ -1,11 +1,15 @@
 package pl.baluch.stickerprinter.data;
 
 import javafx.scene.Cursor;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import pl.baluch.stickerprinter.AppMain;
+import pl.baluch.stickerprinter.Storage;
 import pl.baluch.stickerprinter.events.PrintCellClickedEvent;
 import pl.baluch.stickerprinter.plugins.Item;
 
@@ -15,6 +19,7 @@ public class PrintCell extends AnchorPane {
     private StickerDesign design;
     private Item item;
     private final Pane parent;
+    private ContextMenu contextMenu;
 
     public PrintCell(double x, double y, double w, double h, double scaleRatio, Pane parent) {
         super();
@@ -28,10 +33,26 @@ public class PrintCell extends AnchorPane {
             this.setBackground(new Background(new BackgroundFill(newValue ? Color.color(0, 0, 0, 0.1f) : Color.TRANSPARENT, null, null)));
             this.setCursor(newValue ? Cursor.HAND : Cursor.DEFAULT);
         });
-        this.setOnMouseClicked(event -> AppMain.EVENT_BUS.post(new PrintCellClickedEvent(this)));
+        this.setOnMouseClicked(event -> {
+            contextMenu.hide();
+            if (event.getButton() == MouseButton.PRIMARY) {
+                AppMain.EVENT_BUS.post(new PrintCellClickedEvent(this));
+            }
+        });
         this.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
         this.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+
+        this.contextMenu = new ContextMenu();
+        MenuItem menuItem = new MenuItem(Storage.getResourceBundle().getString("context.menu.delete"));
+        menuItem.setOnAction(event -> setSticker(null, null));
+        contextMenu.getItems().add(menuItem);
+
+        this.setOnContextMenuRequested(event -> {
+            contextMenu.show(this, event.getScreenX(), event.getScreenY());
+            event.consume();
+        });
     }
+
 
     public void setSticker(StickerDesign stickerDesign, Item item) {
         this.design = stickerDesign;
@@ -45,6 +66,10 @@ public class PrintCell extends AnchorPane {
 
     @SuppressWarnings("SuspiciousNameCombination")
     public void drawPreview() {
+        parent.getChildren().removeIf(node -> Objects.equals(node.getId(), "preview-" + this.hashCode()));
+        if(!hasDesign()){
+            return;
+        }
         AnchorPane anchorPane = new AnchorPane();
         anchorPane.setId("preview-" + this.hashCode());
         double width = this.getPrefWidth();
@@ -52,7 +77,7 @@ public class PrintCell extends AnchorPane {
         Rotate rotate = new Rotate();
         Translate translate = new Translate();
         Orientation orientation = width > height ? Orientation.HORIZONTAL : Orientation.VERTICAL;
-        if(orientation != design.getOrientation()){
+        if (orientation != design.getOrientation()) {
             double tmp = width;
             width = height;
             height = tmp;
@@ -68,7 +93,6 @@ public class PrintCell extends AnchorPane {
 
         anchorPane.getTransforms().addAll(rotate, translate);
 
-        parent.getChildren().removeIf(node -> Objects.equals(node.getId(), "preview-" + this.hashCode()));
         parent.getChildren().add(anchorPane);
     }
 }
