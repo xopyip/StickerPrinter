9956fe4d5247a27a5d886f8c5ffdad9363d321b0
xopyip <xopyip@gmail.com>
2022-05-23 22:58:38 +0200

    font property for label

--- src/main/java/pl/baluch/stickerprinter/elements/children/Label.java
+++ src/main/java/pl/baluch/stickerprinter/elements/children/Label.java
@@ -1,23 +1,33 @@
 package pl.baluch.stickerprinter.elements.children;
 
 import com.google.gson.JsonObject;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import pl.baluch.stickerprinter.data.DrawContext;
 import pl.baluch.stickerprinter.data.StickerElementProperty;
 import pl.baluch.stickerprinter.elements.StickerElement;
 
+import java.lang.ref.WeakReference;
+import java.util.Arrays;
+import java.util.Optional;
+
 public class Label extends StickerElement<javafx.scene.control.Label> {
     private final SimpleStringProperty text = new SimpleStringProperty("Test");
+    private final SimpleIntegerProperty fontSize = new SimpleIntegerProperty(15);
+
+    private WeakReference<javafx.scene.control.Label> nodeReference = new WeakReference<>(null);
+
     public Label() {
         super(javafx.scene.control.Label::new);
         addProperty(StickerElementProperty.builder("Text")
                 .value(text.get())
                 .onChange(value -> {
-                    if(value == null){
+                    if (value == null) {
                         return;
                     }
-                    if(value.equals("Custom...")){
+                    if (value.equals("Custom...")) {
                         throw new RuntimeException("Custom text is not supported yet");
                     }
                     text.set(value);
@@ -25,11 +35,28 @@ public class Label extends StickerElement<javafx.scene.control.Label> {
                 //.addChoices() //todo: list all props from item
                 .addChoice("Custom...")
                 .build());
+        addProperty(StickerElementProperty.builder("Font size")
+                .value(fontSize.get() + "px")
+                .onChange(value -> {
+                    if (value == null || !value.endsWith("px")) {
+                        return;
+                    }
+                    try {
+                        int newValue = Integer.parseInt(value.replace("px", ""));
+                        fontSize.set(newValue);
+                        Optional.ofNullable(nodeReference.get()).ifPresent(node -> node.setFont(new Font(newValue)));
+                    } catch (NumberFormatException ignored) {
+                    }
+                })
+                .addChoices(Arrays.asList("5px", "10px", "15px", "20px", "25px", "30px", "35px", "40px", "45px", "50px", "55px", "60px"))
+                .build());
     }
 
     @Override
     public void draw(Pane pane, DrawContext drawContext) {
         javafx.scene.control.Label node = nodeSupplier.get();
+        nodeReference = new WeakReference<>(node);
+        node.setFont(new Font(fontSize.get()));
         node.textProperty().bindBidirectional(text);
         node.setWrapText(false);
         super.setupPositionAndSize(node);
@@ -39,13 +66,17 @@ public class Label extends StickerElement<javafx.scene.control.Label> {
 
     @Override
     public void deserialize(JsonObject properties) {
-        text.set(properties.get("text").getAsString());
+        if (properties.has("text"))
+            text.set(properties.get("text").getAsString());
+        if (properties.has("fontSize"))
+            fontSize.set(properties.get("fontSize").getAsInt());
     }
 
     @Override
     public JsonObject serialize() {
         JsonObject properties = new JsonObject();
         properties.addProperty("text", text.get());
+        properties.addProperty("fontSize", fontSize.get());
         return properties;
     }
 }
